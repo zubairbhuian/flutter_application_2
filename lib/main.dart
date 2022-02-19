201a97d9834111852e7d5e2c0a987285a90bbee0
Zubair <zubairbhuian@gmail.com>
2022-02-19 18:47:03 +0600

    19.3

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
-import './random_words.dart';
-
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        theme: ThemeData(primaryColor: Colors.purple),
-        home: RandomWords());
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+          appBar: AppBar(
+        actions: [
+          ElevatedButton(onPressed: () {}, child: const Icon(Icons.assignment)),
+        ],
+        title: const Text("My App"),
+      )),
+    );
   }
 }
