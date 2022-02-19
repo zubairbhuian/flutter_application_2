aa9e5ab4fc570c9961d718bd3c8374e9c5bc328c
Zubair <zubairbhuian@gmail.com>
2022-02-19 20:34:34 +0600

    19.8

--- lib/main.dart
+++ lib/main.dart
@@ -11,44 +11,38 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        appBar: AppBar(
-          foregroundColor: Colors.grey,
-          actions: [
-            ElevatedButton(
-                onPressed: () {}, child: const Icon(Icons.assignment)),
-          ],
-          title: const Text("My App"),
-          centerTitle: true,
-          leading: TextButton(
-              onPressed: () {},
-              child: const Icon(
-                Icons.add_a_photo,
-                color: Colors.white,
-              )),
-        ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              Container(
-                color: Colors.red,
-                height: 300,
-                width: 200,
-              ),
-              Container(
-                color: const Color.fromARGB(248, 92, 214, 108),
-                height: 300,
-                width: 200,
-              ),
-              Container(
-                color: Color.fromARGB(248, 54, 16, 224),
-                height: 300,
-                width: 200,
-              ),
+          appBar: AppBar(
+            foregroundColor: Colors.grey,
+            actions: [
+              ElevatedButton(
+                  onPressed: () {}, child: const Icon(Icons.assignment)),
             ],
+            title: const Text("My App"),
+            centerTitle: true,
+            leading: TextButton(
+                onPressed: () {},
+                child: const Icon(
+                  Icons.add_a_photo,
+                  color: Colors.white,
+                )),
           ),
-        ),
-      ),
+          body: const Center(
+            child: MyContainer(),
+          )),
+    );
+  }
+}
+
+// Mycontainer
+class MyContainer extends StatelessWidget {
+  const MyContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width / 2.5,
+      height: 50,
+      color: Colors.red,
     );
   }
 }
