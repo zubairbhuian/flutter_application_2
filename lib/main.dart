af84095cee023d071e9b9748870b24646a8c2efe
Zubair <zubairbhuian@gmail.com>
2022-02-19 19:27:52 +0600

    19.5

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,38 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-          appBar: AppBar(
-            foregroundColor: Colors.grey,
-        actions: [
-          ElevatedButton(onPressed: () {}, child: const Icon(Icons.assignment)),
-        ],
-        title: const Text("My App"),
-        centerTitle: true,
-        leading: TextButton(onPressed:(){}, child: const Icon(Icons.add_a_photo,color: Colors.white,)),
-      )),
+        appBar: AppBar(
+          foregroundColor: Colors.grey,
+          actions: [
+            ElevatedButton(
+                onPressed: () {}, child: const Icon(Icons.assignment)),
+          ],
+          title: const Text("My App"),
+          centerTitle: true,
+          leading: TextButton(
+              onPressed: () {},
+              child: const Icon(
+                Icons.add_a_photo,
+                color: Colors.white,
+              )),
+        ),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.red,
+              height: 300,
+            ),
+            Container(
+              color: const Color.fromARGB(248, 92, 214, 108),
+              height: 300,
+            ),
+            Container(
+              color: Color.fromARGB(248, 54, 16, 224),
+              height: 300,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
